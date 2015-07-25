e4388388843fdcc1a404cb06fb9699e5aaa8d33a
Mike Manturov <insolor@users.noreply.github.com>
2015-07-25 19:18:21 +0300

    Implement profile scaling

--- profile_gui.py
+++ profile_gui.py
@@ -6,91 +6,96 @@ class ProfileTk(Profile):
     def __init__(self, **kwargs):
         Profile.__init__(self, **kwargs)
 
-    def canvas_draw(self, parent, x0=0, y0=100, **common):
+    def canvas_draw(self, parent, x0=0, y0=100, scale=10, **common):
         def arc(parent, center, radius, start, extent, **common):
             parent.create_arc((center[0]-radius, center[1]-radius, center[0]+radius, center[1]+radius),
                 start=start, extent=extent, style=ARC, **common)
         
         self.calculate_profile()
         d = []
-        x = x0
+        x = x0-self.width/2
         y = y0
-        # x = -self.width/2
+        
+        # Scale profile parameters
+        b = [x*scale for x in self.b]
+        h1, h2, h3, w1, w2, w3, r1, r3 = (x*scale for x in (self.h1, self.h2, self.h3,
+                                                            self.w1, self.w2, self.w3,
+                                                            self.r1, self.r3))
         
         # Left edge segment B0 (horizontal)
-        if self.b[0] > 0:
-            x1 = x + self.b[0]
-            parent.create_line([(x, y), (x1, y)], **common)
+        if b[0] > 0:
+            x1 = x + b[0]
+            parent.create_line([(x, y), (x1, y)], tag='b0', **common)
             x = x1
         
         for j in range(self.waves):
             # Segment B1 (arc)
-            if self.b[1] > 0:
-                arc(parent, center=(x, y+self.r1), radius=self.r1,
-                    start=90, extent=-self.angle_deg, **common)
-                x += self.w1
-                y += self.h1
+            if b[1] > 0:
+                arc(parent, center=(x, y+r1), radius=r1,
+                    start=90, extent=-self.angle_deg, tag='b1', **common)
+                x += w1
+                y += h1
 
             # Segment B2 (inclined)
-            if self.b[2] > 0:
-                x1 = x + self.w2
-                y1 = y + self.h2
-                parent.create_line([(x, y), (x1, y1)], **common)
+            if b[2] > 0:
+                x1 = x + w2
+                y1 = y + h2
+                parent.create_line([(x, y), (x1, y1)], tag='b2', **common)
                 x = x1
                 y = y1
 
             # Segment B3 (arc)
-            if self.b[3] > 0:
-                x1 = x + self.w3
-                y1 = y + self.h3
-                arc(parent, center=(x1, y1-self.r1), radius=self.r1,
-                    start=270, extent=-self.angle_deg, **common)
+            if b[3] > 0:
+                x1 = x + w3
+                y1 = y + h3
+                arc(parent, center=(x1, y1-r1), radius=r1,
+                    start=270, extent=-self.angle_deg, tag='b3', **common)
                 x = x1
                 y = y1
 
             # Segment B4 (horizontal)
-            if self.b[4] > 0:
-                x1 = x + self.b[4]
-                parent.create_line([(x, y), (x1, y)], **common)
+            if b[4] > 0:
+                x1 = x + b[4]
+                parent.create_line([(x, y), (x1, y)], tag='b4', **common)
                 x = x1
 
             # Symmetrically against B4 segment
             # Segment B3 (arc)
-            if self.b[3] > 0:
-                x1 = x + self.w3
-                y1 = y - self.h3
-                arc(parent, center=(x, y-self.r1), radius=self.r1,
-                    start=270, extent=self.angle_deg, **common)
+            if b[3] > 0:
+                x1 = x + w3
+                y1 = y - h3
+                arc(parent, center=(x, y-r1), radius=r1,
+                    start=270, extent=self.angle_deg, tag='b3', **common)
                 x = x1
                 y = y1
 
             # Segment B2 (inclined)
-            if self.b[2] > 0:
-                x1 = x + self.w2
-                y1 = y - self.h2
-                parent.create_line([(x, y), (x1, y1)], **common)
+            if b[2] > 0:
+                x1 = x + w2
+                y1 = y - h2
+                parent.create_line([(x, y), (x1, y1)], tag='b2', **common)
                 x = x1
                 y = y1
 
             # Segment B1 (arc)
-            if self.b[1] > 0:
-                x1 = x + self.w1
-                y1 = y - self.h1
-                arc(parent, center=(x1, y1+self.r1), radius=self.r1,
-                    start=90, extent=self.angle_deg, **common)
+            if b[1] > 0:
+                x1 = x + w1
+                y1 = y - h1
+                arc(parent, center=(x1, y1+r1), radius=r1,
+                    start=90, extent=self.angle_deg, tag='b1', **common)
                 x = x1
                 y = y1
 
             # Segment B5 (horizontal)
-            if j < self.waves-1 and self.b[5] > 0:
-                x1 = x + self.b[5]
-                parent.create_line([(x, y), (x1, y)], **common)
+            if j < self.waves-1 and b[5] > 0:
+                x1 = x + b[5]
+                parent.create_line([(x, y), (x1, y)], tag='b5', **common)
                 x = x1
 
         # Right edge segment B0
-        if self.b[0] > 0:
-            x1 = x + self.b[0]
-            parent.create_line([(x, y), (x1, y)], **common)
+        if b[0] > 0:
+            x1 = x + b[0]
+            parent.create_line([(x, y), (x1, y)], tag='b0', **common)
 
 
 class App(Tk):
@@ -105,8 +110,9 @@ class App(Tk):
         self._init_main_menu()
         self._init_labels_and_text_boxes(sidebarFrame)
         
-        profile = ProfileTk(b=[20, 5, 20, 5, 20, 20], waves=4, angle_deg=60)
-        profile.canvas_draw(self.canvas, width=1)
+        profile = ProfileTk(b=[20, 20, 20, 20, 20, 20], waves=4, angle_deg=60)
+        profile.canvas_draw(self.canvas, width=2)
+        self.canvas.itemconfig('b4', fill='yellow')
     
     def _init_canvas(self, parent):
         self.canvas = Canvas(parent, width=640, height=480, bg="white")
